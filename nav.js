4aef06f36b0c44542195d65db88ec36c96c85266
Lucy Cai <jcai1@mit.edu>
2025-10-03 17:58:54 -0400

    version controlling site

---
+++ nav.js
@@ -0,0 +1,27 @@
+// nav.js
+window.NAV_HTML = `
+<nav class="navbar">
+  <div class="logo">Lucy Cai</div>
+  <ul class="nav-links">
+    <li><a href="index.html">Home</a></li>
+    <li><a href="projects.html">Projects</a></li>
+    <li><a href="hobbies.html">Hobbies</a></li>
+    <li><a href="https://medium.com/@lucyjcai" target="_blank" rel="noopener">Blog</a></li>
+    <li>
+      <a href="https://www.instagram.com/lucyj_c/" target="_blank" aria-label="Instagram">
+        <i class="fab fa-instagram"></i>
+      </a>
+    </li>
+    <li>
+      <a href="https://x.com/LucyCai9" target="_blank" aria-label="Twitter">
+        <i class="fab fa-twitter"></i>
+      </a>
+    </li>
+    <li>
+      <a href="https://github.com/lucyjcai" target="_blank" aria-label="GitHub">
+        <i class="fab fa-github"></i>
+      </a>
+    </li>
+  </ul>
+</nav>
+`;
