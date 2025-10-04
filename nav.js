1365dca7a0ccdd42f1814130d8eaa992ab1b2ae6
Lucy Cai <jcai1@mit.edu>
2025-10-03 22:41:43 -0400

    added linkedin

--- nav.js
+++ nav.js
@@ -7,13 +7,18 @@ window.NAV_HTML = `
     <li><a href="hobbies.html">Hobbies</a></li>
     <li><a href="https://medium.com/@lucyjcai" target="_blank" rel="noopener">Blog</a></li>
     <li>
-      <a href="https://www.instagram.com/lucyj_c/" target="_blank" aria-label="Instagram">
-        <i class="fab fa-instagram"></i>
+      <a href="https://www.linkedin.com/in/lucy-cai/" target="_blank" aria-label="LinkedIn">
+        <i class="fab fa-linkedin"></i>
       </a>
     </li>
     <li>
       <a href="https://github.com/lucyjcai" target="_blank" aria-label="GitHub">
         <i class="fab fa-github"></i>
+      </a>
+    </li>
+    <li>
+      <a href="https://www.instagram.com/lucyj_c/" target="_blank" aria-label="Instagram">
+        <i class="fab fa-instagram"></i>
       </a>
     </li>
   </ul>
