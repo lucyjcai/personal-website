cb9a0b1632f98cacb7ffe2f3b74b1e91fa4bab01
Lucy Cai <jcai1@mit.edu>
2025-10-03 22:34:10 -0400

    updating style and bio

--- nav.js
+++ nav.js
@@ -1,20 +1,14 @@
 // nav.js
 window.NAV_HTML = `
 <nav class="navbar">
-  <div class="logo">Lucy Cai</div>
   <ul class="nav-links">
-    <li><a href="index.html">Home</a></li>
+    <li><a href="main.html">Home</a></li>
     <li><a href="resume.html">Resume</a></li>
     <li><a href="hobbies.html">Hobbies</a></li>
     <li><a href="https://medium.com/@lucyjcai" target="_blank" rel="noopener">Blog</a></li>
     <li>
       <a href="https://www.instagram.com/lucyj_c/" target="_blank" aria-label="Instagram">
         <i class="fab fa-instagram"></i>
-      </a>
-    </li>
-    <li>
-      <a href="https://x.com/LucyCai9" target="_blank" aria-label="Twitter">
-        <i class="fab fa-twitter"></i>
       </a>
     </li>
     <li>
